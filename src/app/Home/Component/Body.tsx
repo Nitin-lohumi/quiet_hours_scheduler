c2f0bb1d2c2c5431bdc9937c7a8cce07d13b992b
Nitin-Lohumi <lohuminitin@gmail.com>
2025-09-18 10:56:02 +0530

    add

--- src/app/Home/Component/Body.tsx
+++ src/app/Home/Component/Body.tsx
@@ -123,7 +123,9 @@ function Body() {
         ) : (
           <div
             key={task._id}
-            className={`flex shadow-xs ${task.expire&&"bg-red-400"} shadow-green-700 justify-between items-center p-3 rounded-lg`}
+            className={`flex shadow-xs ${
+              task.expire ? "bg-gray-300 shadow-red-400 cursor-not-allowed" : "shadow-green-700 "
+            } justify-between items-center p-3 rounded-lg`}
           >
             <div>
               <p className="font-semibold">{task.task}</p>
